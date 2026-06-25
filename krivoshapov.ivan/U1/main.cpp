8c698e8bb4d57a6f526187a263833ff0721c5be5
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-06-25 11:56:20 +0300

    fix for tests

--- krivoshapov.ivan/U1/main.cpp
+++ krivoshapov.ivan/U1/main.cpp
@@ -1,13 +1,95 @@
-#include "io.hpp"
+#include "../common/io.hpp"
+
+#include <cstring>
+#include <fstream>
 #include <iostream>
 
-int main()
+namespace
 {
+  struct Args
+  {
+    const char *inFile;
+    const char *outFile;
+    bool valid;
+  };
+
+  Args parseArgs(int argc, char **argv)
+  {
+    Args args = {nullptr, nullptr, true};
+    for (int i = 1; i < argc; ++i)
+    {
+      const char *arg = argv[i];
+      if (std::strncmp(arg, "in:", 3) == 0)
+      {
+        if (args.inFile != nullptr)
+        {
+          return {nullptr, nullptr, false};
+        }
+        args.inFile = arg + 3;
+      }
+      else if (std::strncmp(arg, "out:", 4) == 0)
+      {
+        if (args.outFile != nullptr)
+        {
+          return {nullptr, nullptr, false};
+        }
+        args.outFile = arg + 4;
+      }
+      else
+      {
+        return {nullptr, nullptr, false};
+      }
+    }
+    return args;
+  }
+}
+
+int main(int argc, char **argv)
+{
+  if (argc > 3)
+  {
+    std::cerr << "Invalid arguments\n";
+    return 1;
+  }
+  Args args = parseArgs(argc, argv);
+  if (!args.valid)
+  {
+    std::cerr << "Invalid arguments\n";
+    return 1;
+  }
+
+  std::ifstream inStream;
+  std::ofstream outStream;
+
+  if (args.inFile != nullptr)
+  {
+    inStream.open(args.inFile);
+    if (!inStream.is_open())
+    {
+      std::cerr << "Cannot open file: " << args.inFile << '\n';
+      return 2;
+    }
+  }
+  if (args.outFile != nullptr)
+  {
+    outStream.open(args.outFile);
+    if (!outStream.is_open())
+    {
+      std::cerr << "Cannot open file: " << args.outFile << '\n';
+      return 2;
+    }
+  }
+
+  std::istream &in = args.inFile ? static_cast<std::istream &>(inStream) : std::cin;
+  std::ostream &out = args.outFile ? static_cast<std::ostream &>(outStream) : std::cout;
+
   krivoshapov::Vector<krivoshapov::Person> persons;
   krivoshapov::init(persons);
-  krivoshapov::ReadResult r = krivoshapov::readPersons(std::cin, persons);
-  krivoshapov::writePersons(std::cout, persons);
-  std::cerr << r.valid << ' ' << r.ignored << '\n';
+
+  krivoshapov::ReadResult result = krivoshapov::readPersons(in, persons);
+  krivoshapov::writePersons(out, persons);
+  std::cerr << result.valid << ' ' << result.ignored << '\n';
+
   krivoshapov::destroy(persons);
   return 0;
 }
