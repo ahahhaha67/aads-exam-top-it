a8d1f7b5f01d3d4aafb84b0df7f82f6381042c21
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-06-25 11:19:48 +0300

    implement readPerson

--- krivoshapov.ivan/U1/io.cpp
+++ krivoshapov.ivan/U1/io.cpp
@@ -77,3 +77,12 @@ namespace krivoshapov
     }
     return result;
   }
+
+  void writePersons(std::ostream &out, const Vector<Person> &persons)
+  {
+    for (std::size_t i = 0; i < persons.size_; ++i)
+    {
+      out << persons.data_[i].id << ' ' << persons.data_[i].info << '\n';
+    }
+  }
+}
--- krivoshapov.ivan/U1/main.cpp
+++ krivoshapov.ivan/U1/main.cpp
@@ -1,5 +1,12 @@
+#include "io.hpp"
 #include <iostream>
 
 int main()
 {
+  krivoshapov::Vector<krivoshapov::Person> persons;
+  krivoshapov::init(persons);
+  krivoshapov::ReadResult r = krivoshapov::readPersons(std::cin, persons);
+  std::cerr << r.valid << ' ' << r.ignored << '\n';
+  krivoshapov::destroy(persons);
+  return 0;
 }
